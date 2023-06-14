60a780487bd4193bb8ec767d7783ae5b50a9c0c8
Igor <igor01011993@mail.com>
2023-06-15 00:19:11 +0300

    Решение задачи 10

---
+++ Homework_2/Homework_1/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = (number%100)/10;
+if (number/100>0)
+{
+   Console.WriteLine(result); 
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
